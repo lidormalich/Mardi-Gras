98abce6b06e771416115674be3931b17865a7743
Lidor Repo <lidormalich@gmail.com>
2022-11-02 20:53:51 +0200

    update

--- contact.js
+++ contact.js
@@ -1,10 +1,21 @@
-checkBeforSend()
-{
+document.getElementById('theClick').addEventListener('click', go2);
+
+
+function go2() {
+
     const fname = document.getElementById('fname').value;
+    const lname = document.getElementById('lname').value;
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
     const msg = document.getElementById('msg').value;
-    if ((fname == null || email == null || phone == null || msg == null)) {
-        alert("problem");
+    if ((fname == '' || lname == '' || email == '' || phone == '' || msg == '')) {
+        alert("One or more info less");
+        return;
     }
+    let result = email.includes("@");
+    let result2 = email.includes(".");
+    if (!(result && result2)) {
+        alert('mail check problem');
+    }
+
 }
